3b402d8c7464b5d79176906926567871927eb2c6
elvesbd <elvesbd@yahoo.com>
2023-06-18 09:13:58 -0300

    feat: create user object

--- src/controller/create.go
+++ src/controller/create.go
@@ -1,11 +1,22 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/elvesbd/goCrud/src/configuration/restErr"
+	"github.com/elvesbd/goCrud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func Create(c *gin.Context) {
-	err := restErr.NewBadRequestError("Erro na rota")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	err := c.ShouldBindJSON(&userRequest)
+	if err != nil {
+		restErr := restErr.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
